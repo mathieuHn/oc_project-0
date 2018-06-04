18836319f396e23799cfb8e38ec60db3ad51ee7f
mathieuharduin <mathieu.harduin@enedis.fr>
2018-06-04 15:04:14 +0200

    Comment the code in Product Repository

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,8 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
-		products.put(++id, new Product(id, 60, 800.00, "Samsung S9", "Smartphone"));
-		products.put(++id, new Product(id, 70, 900.99, "Iphone X", "smartphone"));
+		products.put(++id, new Product(id, 60, 800.00, "Samsung S9", "Smartphone"));// New product the 04/06/18 : samsung S9
+		products.put(++id, new Product(id, 2, 1900.99, "Iphone X", "smartphone")); // New product the 04/06/18 : iphone X
 	}
 	
 	public List<Product> products() {
